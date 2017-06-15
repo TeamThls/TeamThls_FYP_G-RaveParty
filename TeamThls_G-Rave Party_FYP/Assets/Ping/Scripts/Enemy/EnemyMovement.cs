edab002960f980fedb4620ff9c6890bb757e1967
DESKTOP-FS00L48\zayli <beats0126@gmail.com>
2017-06-15 10:13:06 +0800

    Enemy01 Movement / ShopScene

---
+++ TeamThls_G-Rave Party_FYP/Assets/Ping/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	public float setTime = 0.0f;
+	public float setTime2 = 0.0f;
+	public bool detection = false;
+	public Transform player;
+	Vector3 newPos = new Vector3();
+
+	// Use this for initialization
+	void Start () {
+		//player = GameObject.Find ("player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// detect the player position and store it to a temp;
+		if(detection == false){
+			newPos = player.position;
+			detection = true;
+		}
+
+		// move enemy to the temp position
+		if (detection == true) {
+			transform.position = Vector3.MoveTowards (this.transform.position, newPos, 0.1f);
+		}
+
+		// when enemy reach the temp position, use the timer to reset the bool, bool reseted will redetect the player position
+		if (this.transform.position == newPos) {
+			// set timer to reset the detection on player
+			setTime += Time.deltaTime;
+			setTime2 += Time.deltaTime;
+			int Rand = 0;
+			if (setTime2 >= 0.5f && detection == true) {
+				Rand = Random.Range (1,4);
+			}
+
+			if (Rand == 1) {
+				this.transform.Translate (Vector3.left * 3.0f * Time.deltaTime);
+				//setTime2 = 0.0f;
+			}
+			else if (Rand == 2) {
+				this.transform.Translate (Vector3.right * 3.0f * Time.deltaTime);
+				//setTime2 = 0.0f;
+			}
+			else if (Rand == 3) {
+				this.transform.Translate (Vector3.up * 3.0f * Time.deltaTime);
+				//setTime2 = 0.0f;
+			}
+			else if (Rand == 4) {
+				this.transform.Translate (Vector3.down * 3.0f * Time.deltaTime);
+				//setTime2 = 0.0f;
+			}
+
+			if (setTime >= 3.0f) {
+				detection = false;
+				setTime = 0.0f;
+			}
+		}
+	}
+}
