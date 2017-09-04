f3e1230914e6ddae266f6ca361762304b461fec4
Vycant <xehanort0730@hotmail.com>
2017-09-04 23:36:00 +0800

    Revert "Revert "Dark Room Lightness Recovery, Time Flow Room Slow Mo for both Player(Included Normal Bullet), and Enemy.""
    This reverts commit c536d9a0ae5c9fe14226ccee5ce8447b5b649c13.

--- TeamThls_G-Rave Party_FYP/Assets/Leong/Script/Environment/SoulAbsorb.cs
+++ TeamThls_G-Rave Party_FYP/Assets/Leong/Script/Environment/SoulAbsorb.cs
@@ -7,6 +7,7 @@ public class SoulAbsorb : MonoBehaviour {
 	[SerializeField] GameObject player;
 	Vector2 player_Position;
 	ParticleSystem p_Soul;
+	SharedStats sharedStats;
 	// Use this for initialization
 	void Start () 
 	{
@@ -18,13 +19,17 @@ public class SoulAbsorb : MonoBehaviour {
 		{
 			p_Soul = GetComponent<ParticleSystem>();
 		}
+		if(sharedStats == null)
+		{
+			sharedStats = GameObject.Find("GameManager").GetComponent<SharedStats>();
+		}
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () 
 	{
 		player_Position = new Vector2(player.transform.position.x, player.transform.position.y);
-		transform.position = Vector2.MoveTowards(transform.position, new Vector2(player_Position.x, player_Position.y + 0.2f), Mathf.Lerp(0.01f, 1.0f, 0.1f));
+		transform.position = Vector2.MoveTowards(transform.position, new Vector3(player_Position.x, player_Position.y + 0.2f, player_Position.x - 0.1f), Mathf.Lerp(0.01f, 1.0f, 0.1f));
 	}
 
 	void OnParticleCollision(GameObject obj)
---
+++ TeamThls_G-Rave Party_FYP/Assets/Leong/Script/Environment/TimeFlowRoomEffect.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeFlowRoomEffect : MonoBehaviour {
+
+	TimeManagement time_Script;
+	// Use this for initialization
+	void Start () 
+	{
+		if(time_Script == null)
+		{
+			time_Script = GameObject.Find("TimeManager").GetComponent<TimeManagement>();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		/*if(col.gameObject.layer == 10)
+		{
+			time_Script.SlowDown();
+			Debug.Log("123456");
+		}*/
+	}
+
+	void OnTriggerStay2D(Collider2D col)
+	{
+		time_Script.SlowDown(col.gameObject);
+	}
+
+	void OnTriggerExit2D(Collider2D col)
+	{
+		time_Script.ResetTime(col.gameObject);		
+	}
+}
