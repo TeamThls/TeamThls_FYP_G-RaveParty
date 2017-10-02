3f47185942d268cd483b77b83e16f0aa59b9c2e9
DESKTOP-FS00L48\zayli <beats0126@gmail.com>
2017-10-02 14:00:35 +0800

    flying shooterEnemy movement and combat ( the movement will keep update every 10 sec, can adjusted)

---
+++ TeamThls_G-Rave Party_FYP/Assets/ShootingEnemy.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingEnemy : MonoBehaviour {
+
+	public GameObject Manager;
+	public GameObject Player;
+	public GameObject Player2;
+	public GameObject Target;
+
+	public GameObject bullet;
+
+	SharedStats shrd;
+
+	public float maxX;
+	public float minX;
+	public float maxY;
+	public float minY;
+
+	public float dist;
+	public float targetDist;
+	Vector3 newPos;
+
+	public float targetX;
+
+	public float setTime;
+	public float targetTime;
+	public bool attackMode;
+
+	public float shootDuration;
+	public float maxDuration;
+
+	public float speed;
+	public float step;
+
+	private float a;
+	private float b;
+
+	// Use this for initialization
+	void Start () {
+		Manager = GameObject.Find ("GameManager");
+		Player = GameObject.Find ("Player");
+		Player2 = GameObject.Find ("Player2");
+		shrd = Manager.GetComponent<SharedStats> ();
+		Target = Player;
+		targetDetection ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		targetDetection ();
+		step = speed * Time.deltaTime;
+		setTime += Time.deltaTime;
+		if (setTime >= targetTime) {
+			if (attackMode == false) {
+				attackMode = true;
+				setTime = 0.0f;
+			}
+			else if (attackMode == true) {
+				attackMode = false;
+				setTime = 0.0f;
+			}
+		}
+		if (attackMode == false) {
+			flyingMode ();
+		}
+		if (attackMode == true) {
+			shootDuration += Time.deltaTime;
+			if (shootDuration >= maxDuration) {
+				Instantiate (bullet, this.transform.position, Quaternion.identity);
+				shootDuration = 0.0f;
+			}
+		}
+	}
+
+	void flyingMode()
+	{
+		dist = Vector3.Distance (this.transform.position, Target.transform.position);
+		if (this.transform.position.x >= Target.transform.position.x) {
+			float posX;
+			if (this.transform.position.x + targetX >= maxX) {
+				posX = maxX;
+			} 
+			else {
+				posX = this.transform.position.x + targetX;
+			}
+			newPos = new Vector3 (posX, Target.transform.position.y + 2, this.transform.position.z);
+		}
+		else if (this.transform.position.x < Target.transform.position.x)
+		{
+			float posX;
+			if (Target.transform.position.x - targetX < minX) {
+				posX = minX;
+			} 
+			else {
+				posX = Target.transform.position.x - targetX;
+			}
+			newPos = new Vector3 (posX, Target.transform.position.y + 2, this.transform.position.z);
+		}
+		transform.position = Vector3.MoveTowards (this.transform.position, newPos, step); 
+	}
+
+	void targetDetection()
+	{
+		a = Vector3.Distance (this.transform.position, Player.transform.position);
+		b = Vector3.Distance (this.transform.position, Player2.transform.position);
+
+		if (a > b) {
+			Target = Player2;
+		} 
+		else if (a < b) {
+			Target = Player;
+		}
+	}
+
+}
---
+++ TeamThls_G-Rave Party_FYP/Assets/bulletMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletMovement : MonoBehaviour {
+
+	public GameObject Manager;
+	public GameObject Player;
+	public GameObject Player2;
+	public GameObject Target;
+
+	SharedStats shrd;
+
+	float a;
+	float b;
+
+	public float speed;
+
+	public bool isRight;
+
+	// Use this for initialization
+	void Start () {
+		Manager = GameObject.Find ("GameManager");
+		Player = GameObject.Find ("Player");
+		Player2 = GameObject.Find ("Player2");
+		shrd = Manager.GetComponent<SharedStats> ();
+		Target = Player;
+		targetDetection ();
+		if (this.transform.position.x > Target.transform.position.x) {
+			isRight = false;
+		}
+		else if (this.transform.position.x < Target.transform.position.x) {
+			isRight = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isRight) {
+			this.transform.Translate (Vector3.right * speed * Time.deltaTime);
+		} 
+		else if (!isRight) {
+			this.transform.Translate (Vector3.left * speed * Time.deltaTime);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player") {
+			shrd.player_Health -= 1;
+			Destroy (this.gameObject);
+		}
+	}
+
+	void targetDetection()
+	{
+		a = Vector3.Distance (this.transform.position, Player.transform.position);
+		b = Vector3.Distance (this.transform.position, Player2.transform.position);
+
+		if (a > b) {
+			Target = Player2;
+		} 
+		else if (a < b) {
+			Target = Player;
+		}
+	}
+}
