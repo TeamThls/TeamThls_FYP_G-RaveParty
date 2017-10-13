50913988722102a5d010cca18d6921725124d657
Vycant <xehanort0730@hotmail.com>
2017-10-13 23:17:29 +0800

    Added "Compass" (No 360 rotation, only 8 way)

---
+++ TeamThls_G-Rave Party_FYP/Assets/Leong/Script/Environment/Compass.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Compass : MonoBehaviour {
+
+	public Transform magicBook, healthStation, cameraTransform;
+	[SerializeField] RectTransform magicBookArrow_UITransform, healthStation_UITransform;
+
+
+	void Start () 
+	{
+		cameraTransform = Camera.main.GetComponent<Transform>();
+		if(magicBook == null)
+		{
+			magicBook = GameObject.Find ("MagicSkill").GetComponent<Transform>();
+		}
+		if(healthStation == null)
+		{
+			healthStation = GameObject.Find ("P_HealDevice").GetComponent<Transform>();
+		}
+		if(magicBookArrow_UITransform == null)
+		{
+			magicBookArrow_UITransform = GameObject.Find ("MagicArrow").GetComponent<RectTransform>();
+		}
+		if(healthStation_UITransform == null)
+		{
+			healthStation_UITransform = GameObject.Find ("HealthArrow").GetComponent<RectTransform>();
+		}
+	}
+
+	void FixedUpdate () 
+	{
+		RotateCompass(magicBook, magicBookArrow_UITransform);
+		RotateCompass(healthStation, healthStation_UITransform);
+	}
+	//
+	void RotateCompass(Transform Obj, RectTransform UIImage)
+	{
+		if(Obj.position.x < cameraTransform.position.x)
+		{
+			if(Obj.position.x < cameraTransform.position.x && Obj.position.y > cameraTransform.position.y + 3.0f)
+			{
+				UIImage.rotation = Quaternion.Euler(0, 0, 225);
+			}
+			else if(Obj.position.x < cameraTransform.position.x && Obj.position.y < cameraTransform.position.y - 3.0f)
+			{
+				
+				UIImage.rotation = Quaternion.Euler(0, 0, 315);
+			}
+			else
+			{
+				UIImage.rotation = Quaternion.Euler(0, 0, 270);
+			}
+			Debug.Log("Left");	
+		}
+		else if(Obj.position.x > cameraTransform.position.x)
+		{
+			if(Obj.position.x > cameraTransform.position.x && Obj.position.y < cameraTransform.position.y - 3.0f)
+			{
+				UIImage.rotation = Quaternion.Euler(0, 0, 45);
+			}
+			else if(Obj.position.x > cameraTransform.position.x && Obj.position.y > cameraTransform.position.y + 3.0f)
+			{
+				UIImage.rotation = Quaternion.Euler(0, 0, 135);
+			}
+			else
+			{
+				UIImage.rotation = Quaternion.Euler(0, 0, 90);
+			}
+			Debug.Log("Right");	
+		}
+		else if(Obj.position.y > cameraTransform.position.y)
+		{
+			
+			UIImage.rotation = Quaternion.Euler(0, 0, 180);
+			Debug.Log("Up");	
+		}
+		else if(Obj.position.y < cameraTransform.position.y)
+		{
+			UIImage.rotation = Quaternion.Euler(0, 0, 0);
+			Debug.Log("Down");	
+		}
+	}
+
+
+}
---
+++ TeamThls_G-Rave Party_FYP/Assets/Leong/Script/Environment/HealthParticlesAbsorb.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthParticlesAbsorb : MonoBehaviour {
+
+	[SerializeField] Transform healthCross_Obj;
+	ParticleSystem p_HealthSoul;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if(healthCross_Obj == null)
+		{
+			healthCross_Obj = GameObject.Find("P_HealDevice").GetComponent<Transform>();
+		}
+		p_HealthSoul = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position = Vector2.MoveTowards(transform.position, new Vector3(healthCross_Obj.position.x, healthCross_Obj.position.y + 0.2f, healthCross_Obj.position.x - 0.1f), Mathf.Lerp(0.01f, 1.0f, 0.1f));
+	}
+
+	void OnParticleCollision(GameObject obj)
+	{
+		p_HealthSoul.Stop();
+		StartCoroutine(Vanish(1.0f));
+	}
+
+	IEnumerator Vanish(float duration)
+	{
+		yield return new WaitForSeconds(duration);
+		Destroy(this.gameObject);
+	}
+}
