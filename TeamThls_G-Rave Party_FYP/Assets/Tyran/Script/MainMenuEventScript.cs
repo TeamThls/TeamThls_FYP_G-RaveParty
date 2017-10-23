63f2537502719a328a6872fcbd36505f3bf06fcd
Tyran Yuwen <tyran1296@gmail.com>
2017-10-23 15:27:36 +0800

    Main Menu Changes, got issue with player lighting in main menu as well as collision error, ned to ask leong or han ping on how to fix

---
+++ TeamThls_G-Rave Party_FYP/Assets/Tyran/Script/MainMenuEventScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuEventScript : MonoBehaviour {
+
+	private string scene;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player" ) 
+		{
+			if (Input.GetAxis ("TriangleP1") > 0 || Input.GetAxis ("TriangleP2") > 0 || Input.GetAxis ("TriangleK1") > 0 || Input.GetAxis ("TriangleK2") > 0) 
+			{
+				if (this.gameObject.name == "Play") 
+				{
+					ChangeScene ("Multiplayer Level");
+				}
+				else if (this.gameObject.name == "Leaderboard") 
+				{
+					//ChangeScene()
+				}
+				else if (this.gameObject.name == "Option") 
+				{
+					//ChangeScene()
+				}
+				else if (this.gameObject.name == "Exit") 
+				{
+					Exit();
+				}
+			}
+			
+		}
+	}
+
+	public void ChangeScene (string sceneName)
+	{
+		//SoundManagerScript.Instance.PlaySFX (AudioClipID.SFX_BUTTONPRESSED1);
+		scene = sceneName;
+		Invoke ("ChangeSceneDelay",0.1f);
+	}
+
+	void ChangeSceneDelay ()
+	{
+		SceneManager.LoadScene (scene);
+	}
+
+	public void Exit()
+	{
+		//SoundManagerScript.Instance.PlaySFX (AudioClipID.SFX_BUTTONPRESSED1);
+		Application.Quit();
+	}
+}
