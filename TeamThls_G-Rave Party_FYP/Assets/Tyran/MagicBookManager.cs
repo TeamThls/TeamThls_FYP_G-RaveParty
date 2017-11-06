11090573587748cedcd40e18ab09147da01820df
Tyran Yuwen <tyran1296@gmail.com>
2017-11-06 15:55:29 +0800

    Magic SkilBook Randomized Position. To edit anything, look for magic book parent

---
+++ TeamThls_G-Rave Party_FYP/Assets/Tyran/MagicBookManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicBookManager : MonoBehaviour 
+{
+	public float setTime = 0.0f;
+	public float currTime = 0.0f;
+	public int r;
+	public GameObject magicSkillBook;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (currTime >= setTime) 
+		{
+			r = Random.Range (0, 8);
+			magicSkillBook.transform.parent = this.transform.GetChild (r); 
+			magicSkillBook.transform.position = magicSkillBook.transform.parent.position;
+			currTime = 0.0f;
+
+		} 
+		else 
+		{
+			currTime += Time.deltaTime;
+		}
+		
+	}
+}
