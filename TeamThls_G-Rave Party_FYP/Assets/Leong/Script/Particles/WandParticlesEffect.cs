444d055e0fd0b61d2c3bb634cf3a681d9b2f67b9
Vycant <xehanort0730@hotmail.com>
2017-12-06 14:44:21 +0800

    Added Lights to all wand particles, Resize the level objects size (much thinner now), Readjust some lights' position, Change Sync Particles.

--- TeamThls_G-Rave Party_FYP/Assets/Leong/Script/Particles/WandParticlesEffect.cs
+++ TeamThls_G-Rave Party_FYP/Assets/Leong/Script/Particles/WandParticlesEffect.cs
@@ -6,6 +6,7 @@ public class WandParticlesEffect : MonoBehaviour {
 
 	ParticleSystem normal_Ps, fire_Ps, ice_Ps, laser_Ps;
 	[SerializeField] ParticleSystem current_Ps;
+	[SerializeField] Light particles_Light;
 	SharedStats sharedStats_Script;
 	// Use this for initialization
 	void Start () 
@@ -14,6 +15,7 @@ public class WandParticlesEffect : MonoBehaviour {
 		fire_Ps = transform.GetChild(1).GetComponent<ParticleSystem>();
 		ice_Ps = transform.GetChild(2).GetComponent<ParticleSystem>();
 		laser_Ps = transform.GetChild(3).GetComponent<ParticleSystem>();
+		particles_Light = transform.GetChild(4).GetComponent<Light>();
 
 		current_Ps = normal_Ps;
 		sharedStats_Script = GameObject.Find("GameManager").GetComponent<SharedStats>();
@@ -27,14 +29,13 @@ public class WandParticlesEffect : MonoBehaviour {
 
 	void ChangeWandType()
 	{
-		
 		if(sharedStats_Script.OnFire == true)
 		{
 			normal_Ps.Stop();
 			ice_Ps.Stop();
 			laser_Ps.Stop();
 			current_Ps = fire_Ps;
-
+			particles_Light.color = Color.yellow;
 		}
 		else if(sharedStats_Script.OnIce == true)
 		{
@@ -42,7 +43,7 @@ public class WandParticlesEffect : MonoBehaviour {
 			fire_Ps.Stop();
 			laser_Ps.Stop();
 			current_Ps = ice_Ps;
-
+			particles_Light.color = new Color32(238, 130, 238, 255);
 		}
 		else if(sharedStats_Script.OnLaser == true)
 		{
@@ -50,6 +51,7 @@ public class WandParticlesEffect : MonoBehaviour {
 			ice_Ps.Stop();
 			fire_Ps.Stop();
 			current_Ps = laser_Ps;
+			particles_Light.color = Color.green;
 
 		}
 		else
@@ -58,6 +60,7 @@ public class WandParticlesEffect : MonoBehaviour {
 			ice_Ps.Stop();
 			laser_Ps.Stop();
 			current_Ps = normal_Ps;
+			particles_Light.color = Color.white;
 		}
 		if(current_Ps.isPlaying == false)
 		{
